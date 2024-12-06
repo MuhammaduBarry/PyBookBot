34c37b0a999d57f02d20e6a00ce72b9681a839a0
github_MuhammaduBarry <Muhammadubarry5@gmail.com>
2024-12-06 13:51:19 -0500

    Finsihed features

--- main.py
+++ main.py
@@ -22,8 +22,34 @@ def count_char():
 
     return book_dict
 
+def sort_num(d):
+    for key, value in d.items():
+        return value
+
+
+def print_report():
+    word_count_dict = count_char()
+    list_of_words = []
+
+    # Loop through dictionary
+    for key, value in word_count_dict.items():
+        if key.isalpha():
+            list_of_words.append({key:value})
+
+
+    list_of_words.sort(reverse=True, key=sort_num)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count(book_path)} words found in the document")
+    # Loop through list which contains dictionaries
+    for word in list_of_words:
+        for key, value in word.items():
+            print(f"The '{key}' character was found {value} times")
+    print("--- End report ---")
+
+
 def main():
-    count_char()
+    print_report()
 
 main()
 
